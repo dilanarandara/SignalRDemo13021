e1889c6427c5a730cc4c80b71c962d0de2d80335
Dilan Arandara <dilan.madu@gmail.com>
2013-04-04 14:11:33 +0530

    Finalized the code.

---
+++ SignalRDemo/Scripts/Notification.js
@@ -0,0 +1,19 @@
+﻿/// <reference path="jquery-1.6.4.js" />
+/// <reference path="jquery.signalR-1.0.0-rc2.min.js" />
+
+
+$(function () {
+    var hub = $.connection.notification;
+
+    hub.client.sendDetails = function (data) {
+        $('#messages').append(data + "<br />");
+    };
+
+    $.connection.hub.start().done(function () {
+        $('#SendNotification').click(function () {
+            var message = $('#txtNotification').val();
+            hub.server.sendNotifications(message);
+            $('#txtNotification').val('');
+        });
+    });
+});
